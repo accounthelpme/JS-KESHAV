6b16f65539404a6b4fa524908df7612436d75668
accounthelpme <160570569+accounthelpme@users.noreply.github.com>
2024-11-16 08:24:23 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as neweer version
+// alert(3+3) // we are using nodejs not browser
+// alert(3+
++ 3 // not readable , code readability should be high
+
+let name = " string"
+let number = 223213
+let isloggedin = true
+// number => 2
+// string=> ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+//  symbol => to fine uniqueness in the code
+// object
+console.log(typeof "string");
+console.log(typeof 223213);
+console.log(typeof true);
+
+
+
+
+
