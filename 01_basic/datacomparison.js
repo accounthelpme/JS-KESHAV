791e85d047f54b62e3a1c139f70143ee0526c23d
accounthelpme <160570569+accounthelpme@users.noreply.github.com>
2024-11-16 12:18:40 +0000

    DATACOMAPRISON

---
+++ 01_basic/datacomparison.js
@@ -0,0 +1,32 @@
+// console.log(2>1);
+// console.log(2 == 1);
+// console.log(2 >= 1);
+// console.log(2<1);
+// console.log(2 != 1);
+
+// console.log("2">1);
+// console.log("02">1);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined<0);
+// we have to avoide using null  and uNDEFINE AS THEY CAN CAUSE CONFUSION
+// ===
+
+console.log("2"==="2" );
+
+
+
+
+
+
+
+
+
+
+
+
+
