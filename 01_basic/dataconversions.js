75959a91e8680b7abee3a37203803ad4daeadf3f
accounthelpme <160570569+accounthelpme@users.noreply.github.com>
2024-11-16 08:54:31 +0000

    conversion of datatype

---
+++ 01_basic/dataconversions.js
@@ -0,0 +1,25 @@
+let score  = 33
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueINNumber = Number(score)
+//  console.log(typeof valueINNumber);
+//  console.log(valueINNumber);
+
+//  "33" => 33 //
+//  "33abc" => NaN//
+//  TRUE =>1; False => 0//
+ 
+let tuthu = "tumera"
+let BOOLEANNAME = Boolean(tuthu) 
+//  console.log(typeof BOOLEANNAME)
+//  console.log( BOOLEANNAME);
+
+//  1 => true; 0 => false
+// " " => false
+// "hitesh" => true
+ 
+let stringtunem = 33
+let tumernam = String(stringtunem)
+console.log(typeof tumernam);
