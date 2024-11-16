351c9a760c9565cb809c0d52632f300964e545d7
accounthelpme <160570569+accounthelpme@users.noreply.github.com>
2024-11-16 08:24:52 +0000

    variable

---
+++ 01_basic/variable.js
@@ -0,0 +1,10 @@
+const accountid = 1435362
+let accountemail = "keshavchanderdogra@gmail.com"
+var accountpassoword= "992005sep"
+let acountstate ; 
+accountpassoword = 23322211
+console.table([accountid, accountemail,accountpassoword,acountstate])
+
+
+
+
